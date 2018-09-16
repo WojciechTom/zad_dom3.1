851ecd70d8a7aee58b89844ec073e99cded8b7fe
Wojciech T <hadwao2@gmail.com>
2018-09-16 18:12:47 +0200

    zad 3.3

---
+++ src/telewizor/Televisor.java
@@ -0,0 +1,24 @@
+package telewizor;
+
+public class Televisor {
+
+    String  offOrOn ;
+
+
+    Televisor (){
+        offOrOn = "OFF";
+    }
+
+
+    void turnOn(){
+    offOrOn = "ON";
+    }
+
+    void turnOff (){
+    offOrOn = "OFF";
+    }
+
+    void ShowStatus(){
+        System.out.println("TV is " + offOrOn);
+    }
+}
---
+++ src/telewizor/TelevisorTest.java
@@ -0,0 +1,13 @@
+package telewizor;
+
+public class TelevisorTest {
+    public static void main(String[] args) {
+    Televisor tv = new Televisor();
+
+    tv.ShowStatus();
+    tv.turnOn();
+    tv.ShowStatus();
+    tv.turnOff();
+    tv.ShowStatus();
+    }
+}
